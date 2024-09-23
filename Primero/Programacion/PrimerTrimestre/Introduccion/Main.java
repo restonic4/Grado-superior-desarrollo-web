ef3dc611f52c00297d4f6686a9470846c0415604
restonic4 <marcosruizlerma@gmail.com>
2024-09-23 09:36:29 +0200

    Introduccion

--- Primero/Programacion/PrimerTrimestre/Introduccion/Main.java
+++ Primero/Programacion/PrimerTrimestre/Introduccion/Main.java
@@ -1,7 +1,28 @@
 package Primero.Programacion.PrimerTrimestre.Introduccion;
 
+// I already know all of this xd
 public class Main {
+    /**
+     * This is a javadoc, because why not
+     * @param args Console params
+     */
     public static void main(String[] args) {
+        // Real numbers
+        byte veryShortNumber = 127;
+        short shortNumber = 32767;
+        int number = 2147483647;
+        long longNumber = 99999999999L;
 
+        // Decimal numbers
+        float decimalNumber = 0.75f;
+        double bigDecimalNumber = 0.7568734253498d;
+
+        // Other types
+        boolean condition = true;
+        char character = 'r';
+
+        String text = "Java my beloved";
+
+        System.out.println(text);
     }
 }
