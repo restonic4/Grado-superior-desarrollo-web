7db22988a74f106a08785a839b9c425633b87375
restonic4 <marcosruizlerma@gmail.com>
2024-09-25 12:16:47 +0200

    Ejercicio 3

---
+++ Primero/Programacion/PrimerTrimestre/Ejercicio3/Main.java
@@ -0,0 +1,16 @@
+package Primero.Programacion.PrimerTrimestre.Ejercicio3;
+
+import java.util.Scanner;
+
+@SuppressWarnings("all")
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age;
+
+        System.out.print("Introduza su edad: ");
+        age = scanner.nextInt();
+
+        System.out.println("Su edad es: " + age + ", el año que viene tendras " + (age + 1) + " años.");
+    }
+}
