45ded3d4aae563b5e5d65b30719f8d5feabcb991
restonic4 <marcosruizlerma@gmail.com>
2024-09-25 12:57:24 +0200

    Ejercicio 4

---
+++ Primero/Programacion/PrimerTrimestre/Ejercicio4/Main.java
@@ -0,0 +1,21 @@
+package Primero.Programacion.PrimerTrimestre.Ejercicio4;
+
+import java.util.Scanner;
+
+@SuppressWarnings("all")
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age;
+
+        System.out.print("Introduce tu edad: ");
+        age = scanner.nextInt();
+
+        if (age >= 18) {
+            System.out.println("Mayor de edad");
+            return;
+        }
+
+        System.out.println("Tas chikito");
+    }
+}
